da03d0a4862395e91f1cbb9028acc67a5e99cff0
charlescleyton <charles.pereira.ti@gmail.com>
2021-04-13 17:26:01 -0300

    Retorno e validação de inclusão de convidado

---
+++ eventosapp/src/main/java/com/eventosapp/repository/ConvidadoRepository.java
@@ -0,0 +1,12 @@
+package com.eventosapp.repository;
+
+import com.eventosapp.model.Convidado;
+import com.eventosapp.model.Evento;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, String> {
+
+    Iterable<Convidado> findByEvento(Evento evento);
+
+}
