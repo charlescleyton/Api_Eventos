6c7f8fb1f37c32b8c83db6b1e3eec69d4510fea5
charlescleyton <charles.pereira.ti@gmail.com>
2021-04-13 14:53:06 -0300

    Vizualizar Detalhes do Evento

--- eventosapp/src/main/java/com/eventosapp/controller/EventoController.java
+++ eventosapp/src/main/java/com/eventosapp/controller/EventoController.java
@@ -5,6 +5,7 @@ import com.eventosapp.repository.EventoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,6 +36,14 @@ public class EventoController {
         Iterable<Evento> evento = er.findAll();
         mv.addObject("eventos", evento);
 
+        return mv;
+    }
+
+    @RequestMapping("/{codigo}")
+    public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
+        Evento evento = er.findByCodigo(codigo);
+        ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+        mv.addObject("evento", evento);
         return mv;
     }
 }
